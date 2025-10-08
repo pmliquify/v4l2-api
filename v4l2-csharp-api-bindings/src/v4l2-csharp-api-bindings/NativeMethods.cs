bb266cfb55b4e8ab61e1d4fc988a5481295fba7f
Konstantin Wachendorff <kw@eyev.de>
2025-10-08 14:27:08 +0200

    updated c# pinvoke v4l2 driver now decides the buffer index

--- v4l2-csharp-api-bindings/src/v4l2-csharp-api-bindings/NativeMethods.cs
+++ v4l2-csharp-api-bindings/src/v4l2-csharp-api-bindings/NativeMethods.cs
@@ -2,7 +2,7 @@ using System;
 using System.Runtime.InteropServices;
 
 namespace v4l2_csharp_api_bindings;
-public class V4L2ImageInterop : IDisposable
+public partial class V4L2ImageInterop : IDisposable
 {
     public readonly IntPtr _handle;
 
@@ -15,6 +15,7 @@ public class V4L2ImageInterop : IDisposable
     public uint BytesUsed => bytesUsed(_handle);
     public uint PlanesCount => planesCount(_handle);
     public ushort Shift => shift(_handle);
+    public ushort BufferIndex => bufferIndex(_handle);
 
     public IntPtr PlaneAt(uint index = 0) => planeAt(_handle, index);
 
@@ -45,62 +46,65 @@ public class V4L2ImageInterop : IDisposable
 
     #region NativeMethods
     private const string LibraryName = "libv4l2-c-api.so";
-    [DllImport(LibraryName, EntryPoint = "Image_create")]
-    private static extern IntPtr create();
+    [LibraryImport(LibraryName, EntryPoint = "Image_create")]
+    private static partial IntPtr create();
 
-    [DllImport(LibraryName, EntryPoint = "Image_destroy")]
-    private static extern void destroy(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_destroy")]
+    private static partial void destroy(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_pixelValue")]
-    private static extern ushort pixelValue(IntPtr imagePtr, ushort x, ushort y);
+    [LibraryImport(LibraryName, EntryPoint = "Image_pixelValue")]
+    private static partial ushort pixelValue(IntPtr imagePtr, ushort x, ushort y);
 
-    [DllImport(LibraryName, EntryPoint = "Image_init")]
-    private static extern void init(IntPtr imagePtr, ushort width, ushort height, ushort bytesPerLine, uint imageSize,
+    [LibraryImport(LibraryName, EntryPoint = "Image_init")]
+    private static partial void init(IntPtr imagePtr, ushort width, ushort height, ushort bytesPerLine, uint imageSize,
                                          uint bytesUsed, uint pixelformat, uint sequence, ulong timestamp);
 
-    [DllImport(LibraryName, EntryPoint = "Image_width")]
-    private static extern ushort width(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_width")]
+    private static partial ushort width(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_height")]
-    private static extern ushort height(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_height")]
+    private static partial ushort height(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_bytesPerLine")]
-    private static extern ushort bytesPerLine(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_bytesPerLine")]
+    private static partial ushort bytesPerLine(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_imageSize")]
-    private static extern uint imageSize(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_imageSize")]
+    private static partial uint imageSize(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_setImageSize")]
-    private static extern void setImageSize(IntPtr imagePtr, uint size);
+    [LibraryImport(LibraryName, EntryPoint = "Image_setImageSize")]
+    private static partial void setImageSize(IntPtr imagePtr, uint size);
 
-    [DllImport(LibraryName, EntryPoint = "Image_bytesUsed")]
-    private static extern uint bytesUsed(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_bytesUsed")]
+    private static partial uint bytesUsed(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_pixelformat")]
-    private static extern uint pixelformat(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_pixelformat")]
+    private static partial uint pixelformat(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_sequence")]
-    private static extern uint sequence(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_sequence")]
+    private static partial uint sequence(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_timestamp")]
-    private static extern ulong timestamp(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_timestamp")]
+    private static partial ulong timestamp(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_shift")]
-    private static extern ushort shift(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_shift")]
+    private static partial ushort shift(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_setShift")]
-    private static extern void setShift(IntPtr imagePtr, ushort shift);
+    [LibraryImport(LibraryName, EntryPoint = "Image_setShift")]
+    private static partial void setShift(IntPtr imagePtr, ushort shift);
 
-    [DllImport(LibraryName, EntryPoint = "Image_planesCount")]
-    private static extern uint planesCount(IntPtr imagePtr);
+    [LibraryImport(LibraryName, EntryPoint = "Image_planesCount")]
+    private static partial uint planesCount(IntPtr imagePtr);
 
-    [DllImport(LibraryName, EntryPoint = "Image_planeAt")]
-    private static extern IntPtr planeAt(IntPtr imagePtr, uint index);
+    [LibraryImport(LibraryName, EntryPoint = "Image_planeAt")]
+    private static partial IntPtr planeAt(IntPtr imagePtr, uint index);
+
+    [LibraryImport(LibraryName, EntryPoint = "Image_bufferIndex")]
+    private static partial ushort bufferIndex(IntPtr imagePtr);
     #endregion NativeMethods
 
 }
 
-public class V4L2VideoDeviceInterop : IDisposable
+public partial class V4L2VideoDeviceInterop : IDisposable
 {
     public readonly IntPtr _handle;
     public V4L2VideoDeviceInterop()
@@ -121,13 +125,15 @@ public class V4L2VideoDeviceInterop : IDisposable
     public int SetSelection(int left, int top, int width, int height) => setSelection(_handle, left, top, width, height);
 
     public int StreamOn(int bufferCount = 3) => streamOn(_handle, bufferCount);
-    
+
     public int StreamOff() => streamOff(_handle);
 
     // the timeout is 1000000 microseconds = 1 second
     public int GetNextImage(out IntPtr imagePtr, int timeout = 1000000, bool lastImage = false) => getNextImage(_handle, out imagePtr, timeout, lastImage);
 
     public int ReleaseImage(V4L2ImageInterop image) => releaseImage(_handle, image._handle);
+
+    public int ReleaseImageByIndex(int bufferIndex) => releaseImageByIndex(_handle, bufferIndex);
 
     public int GetImage(out IntPtr imagePtr, int timeout, bool lastImage = false) => getImage(_handle, out imagePtr, timeout, lastImage);
 
@@ -155,73 +161,78 @@ public class V4L2VideoDeviceInterop : IDisposable
     }
 
     #region NativeMethods
+    
     private const string LibraryName = "libv4l2-c-api.so";
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_create")]
-    private static extern IntPtr create();
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_create")]
+    private static partial IntPtr create();
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_destroy")]
-    private static extern void destroy(IntPtr videoDevicePtr);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_destroy")]
+    private static partial void destroy(IntPtr videoDevicePtr);
 
     [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_open")]
     private static extern int open(IntPtr videoDevicePtr, string devicePath, string subDevicePath);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_close")]
-    private static extern int close(IntPtr videoDevicePtr);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_close")]
+    private static partial int close(IntPtr videoDevicePtr);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_getFormat")]
-    private static extern int getFormat(IntPtr videoDevicePtr);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_getFormat")]
+    private static partial int getFormat(IntPtr videoDevicePtr);
 
     [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setFormat")]
     private static extern int setFormat(IntPtr videoDevicePtr, string pixelFormat);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_printFormat")]
-    private static extern int printFormat(IntPtr videoDevicePtr);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_printFormat")]
+    private static partial int printFormat(IntPtr videoDevicePtr);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setSelection")]
-    private static extern int setSelection(IntPtr videoDevicePtr, int left, int top, int width, int height);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setSelection")]
+    private static partial int setSelection(IntPtr videoDevicePtr, int left, int top, int width, int height);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_streamOn")]
-    private static extern int streamOn(IntPtr videoDevicePtr, int bufferCount);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_streamOn")]
+    private static partial int streamOn(IntPtr videoDevicePtr, int bufferCount);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_streamOff")]
-    private static extern int streamOff(IntPtr videoDevicePtr);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_streamOff")]
+    private static partial int streamOff(IntPtr videoDevicePtr);
 
     [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_getNextImage")]
     private static extern int getNextImage(IntPtr videoDevicePtr, out IntPtr image, int timeout, bool lastImage = false);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_releaseImage")]
-    private static extern int releaseImage(IntPtr videoDevicePtr, IntPtr image);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_releaseImage")]
+    private static partial int releaseImage(IntPtr videoDevicePtr, IntPtr image);
+
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_releaseImageByIndex")]
+    private static partial int releaseImageByIndex(IntPtr videoDevicePtr, int bufferIndex);
 
     [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_getImage")]
     private static extern int getImage(IntPtr videoDevicePtr, out IntPtr image, int timeout, bool lastImage = false);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setExposure")]
-    private static extern int setExposure(IntPtr videoDevicePtr, int exposure);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setExposure")]
+    private static partial int setExposure(IntPtr videoDevicePtr, int exposure);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setGain")]
-    private static extern int setGain(IntPtr videoDevicePtr, int gain);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setGain")]
+    private static partial int setGain(IntPtr videoDevicePtr, int gain);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setBlackLevel")]
-    private static extern int setBlackLevel(IntPtr videoDevicePtr, int blackLevel);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setBlackLevel")]
+    private static partial int setBlackLevel(IntPtr videoDevicePtr, int blackLevel);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setBinning")]
-    private static extern int setBinning(IntPtr videoDevicePtr, int binning);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setBinning")]
+    private static partial int setBinning(IntPtr videoDevicePtr, int binning);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setTriggerMode")]
-    private static extern int setTriggerMode(IntPtr videoDevicePtr, int triggerMode);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setTriggerMode")]
+    private static partial int setTriggerMode(IntPtr videoDevicePtr, int triggerMode);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setIOMode")]
-    private static extern int setIOMode(IntPtr videoDevicePtr, int ioMode);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setIOMode")]
+    private static partial int setIOMode(IntPtr videoDevicePtr, int ioMode);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setFrameRate")]
-    private static extern int setFrameRate(IntPtr videoDevicePtr, int frameRate);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setFrameRate")]
+    private static partial int setFrameRate(IntPtr videoDevicePtr, int frameRate);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setControlById")]
-    private static extern int setControl(IntPtr videoDevicePtr, uint id, int value);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setControlById")]
+    private static partial int setControl(IntPtr videoDevicePtr, uint id, int value);
 
-    [DllImport(LibraryName, EntryPoint = "V4L2VideoDevice_setControlByName")]
-    private static extern int setControl(IntPtr videoDevicePtr, string name, int value);
+    [LibraryImport(LibraryName, EntryPoint = "V4L2VideoDevice_setControlByName", StringMarshalling = StringMarshalling.Utf8)]
+    private static partial int setControl(IntPtr videoDevicePtr, string name, int value);
+    
     #endregion NativeMethods
 
 }
